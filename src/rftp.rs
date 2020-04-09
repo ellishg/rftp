fddbcc093309ff1cb0dc44f7a427be81492a70de
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2020-04-08 17:46:34 -0700

    Increase timeout

--- src/rftp.rs
+++ src/rftp.rs
@@ -261,7 +261,7 @@ fn create_session(
     };
 
     let mut session = ssh2::Session::new()?;
-    session.set_timeout(3000);
+    session.set_timeout(10000);
     session.set_compress(true);
     session.set_tcp_stream(tcp);
     session.handshake()?;
