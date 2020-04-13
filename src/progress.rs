19481e44fce0f792680cd9b291eab216ed80f28d
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2020-04-13 14:51:18 -0700

    Move tests to utils

--- src/progress.rs
+++ src/progress.rs
@@ -209,32 +209,8 @@ impl Progress {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::utils::*;
+    use crate::utils::buffer_without_style;
     use tui::{backend::TestBackend, buffer::Buffer, Terminal};
-
-    #[test]
-    fn test_duration() {
-        assert_eq!(
-            duration_to_string(Duration::from_secs(12)),
-            "00:12".to_string()
-        );
-        assert_eq!(
-            duration_to_string(Duration::from_secs(123)),
-            "02:03".to_string()
-        );
-        assert_eq!(
-            duration_to_string(Duration::from_secs(2 * 60 * 60 + 40 * 60 + 59)),
-            "2:40:59".to_string()
-        );
-    }
-
-    #[test]
-    fn test_bitrate() {
-        assert_eq!(bitrate_to_string(4), "4 bit/s".to_string());
-        assert_eq!(bitrate_to_string(1e3 as u64), "1.0 Kbit/s".to_string());
-        assert_eq!(bitrate_to_string(3e6 as u64), "3.0 Mbit/s".to_string());
-        assert_eq!(bitrate_to_string(7e9 as u64), "7.0 Gbit/s".to_string());
-    }
 
     #[test]
     fn test_draw_progress() {
